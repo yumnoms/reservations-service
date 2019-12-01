261cf0b440ce8a1dd0298314c2a2a4d88dcfcdb5
Laurence <ldn221@nyu.edu>
2019-12-01 00:09:12 -0800

    Set up mysql database connection and schema

---
+++ server/database.js
@@ -0,0 +1,53 @@
+const Sequelize = require('sequelize');
+const seed = require('./seedDatabase.js');
+
+
+const sequelize = new Sequelize('YumNoms', 'student', 'student', {
+  dialect: 'mysql',
+  port: 3306,
+});
+
+const Restaurant = sequelize.define('Restaurant', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  name: Sequelize.STRING,
+  min_party: Sequelize.INTEGER,
+  max_party: Sequelize.INTEGER,
+  open: Sequelize.TIME,
+  close: Sequelize.TIME,
+}, { underscored: true });
+
+const Table = sequelize.define('Table', {
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  restaurant_id: Sequelize.INTEGER,
+  date: Sequelize.DATEONLY,
+  time: Sequelize.TIME,
+  min_seating: Sequelize.INTEGER,
+  max_seating: Sequelize.INTEGER,
+  is_open: { type: Sequelize.BOOLEAN, defaultValue: true },
+}, { underscored: true });
+
+Table.belongsTo(Restaurant);
+Restaurant.hasMany(Table, { as: 'Offerings' });
+
+
+module.exports.initialize = () => {
+  sequelize.authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.');
+      return sequelize.sync({ force: true });
+    })
+    .then(() => {
+      console.log('Database tables synced!');
+      return seed.restaurants(Restaurant);
+    })
+    .then(() => {
+      console.log('Successfully seeded Restaurants!');
+      return seed.tables(Table, Restaurant);
+    })
+    .then(() => {
+      console.log('Successfully seeded Tables!');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};

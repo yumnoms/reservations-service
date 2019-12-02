b449bd8366bf4aed071ce17f7bad28f6adf46306
Laurence <ldn221@nyu.edu>
2019-12-01 16:06:33 -0800

    Fix bug in seeding function promises

--- server/seedDatabase.js
+++ server/seedDatabase.js
@@ -1,4 +1,3 @@
-// const Sequelize = require('sequelize');
 const faker = require('faker');
 const db = require('./database.js');
 
@@ -31,27 +30,29 @@ const seedTables = () => {
   for (let i = 0; i < numberOfTables; i += 1) {
     const restaurantId = faker.random.number({ min: 1, max: numberOfRestaurants });
 
-    db.Restaurant.findOne({ where: { id: restaurantId } })
-      .then((restaurant) => {
-        const info = restaurant.dataValues;
-        const tableSize = 2 * faker.random.number({ min: 1, max: (info.max_party / 2) });
-        let startTime = `${faker.random.number({ min: 9, max: 23 })}:${faker.random.arrayElement(['00', '15', '30', '45'])}:00`;
+    promises.push(
+      db.Restaurant.findOne({ where: { id: restaurantId } })
+        .then((restaurant) => {
+          const info = restaurant.dataValues;
+          const tableSize = 2 * faker.random.number({ min: 1, max: (info.max_party / 2) });
+          let startTime = `${faker.random.number({ min: 9, max: 23 })}:${faker.random.arrayElement(['00', '15', '30', '45'])}:00`;
 
-        while (startTime < info.open && startTime > info.close) {
-          startTime = `${faker.random.number({ min: 9, max: 23 })}:${faker.random.arrayElement(['00', '15', '30', '45'])}:00`;
-        }
+          while (startTime < info.open && startTime > info.close) {
+            startTime = `${faker.random.number({ min: 9, max: 23 })}:${faker.random.arrayElement(['00', '15', '30', '45'])}:00`;
+          }
 
-        const newEntry = {
-          restaurant_id: restaurantId,
-          date: faker.date.recent(-7),
-          time: startTime,
-          min_seating: tableSize - 1,
-          max_seating: tableSize,
-          is_open: faker.random.boolean(),
-        };
+          const newEntry = {
+            restaurant_id: restaurant.dataValues.id,
+            date: faker.date.recent(-7),
+            time: startTime,
+            min_seating: tableSize - 1,
+            max_seating: tableSize,
+            is_open: faker.random.boolean(),
+          };
 
-        promises.push(db.Table.create(newEntry));
-      });
+          return db.Table.create(newEntry);
+        }),
+    );
   }
 
   return Promise.all(promises);

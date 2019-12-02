0a8e258c477745d98d905150b636c07b5e73df0e
Laurence <ldn221@nyu.edu>
2019-12-01 16:07:40 -0800

    Implement server endpoint for getting restaurantInfo object
    - express server endpoint routes to call of controller.getInfo
    - controller.getInfo handles call to model.getInfo and model.getTables to create and return restaurantInfo object
    - model getInfo and getTables query mysql database tables for entries to pass to controller to create restaurantInfo object

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const controller = require('./controller.js');
 const db = require('./database.js');
 
 
@@ -10,19 +11,37 @@ server.use(bodyParser.urlencoded({ urlencoded: true }));
 server.use(express.static(path.join(__dirname, '../public')));
 
 
-// server.get('/api/:id', (req, res) => {
+server.get('/api/:id', (req, res) => {
+  controller.getInfo(req.params.id)
+    .then((info) => {
+      res.status(200).send(info);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
-// });
+
+server.get('/api/:id/search', (req, res) => {
+  controller.getTables(req.params.id, req.query)
+    .then((tables) => {
+      res.status(200).send(tables);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 
-// server.get('/api/:id/search', (req, res) => {
-
-// });
-
-
-// server.post('/api/:id/reservation', (req, res) => {
-
-// });
+server.post('/api/:id/reservation', (req, res) => {
+  controller.makeReservation(req.params.id, req.body.table)
+    .then((table) => {
+      res.status(200).send(table);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 
 const port = 3000;

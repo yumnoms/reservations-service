5fd8f2e54a831a3cf9f57a46e1cb9250f6dbb762
Laurence <ldn221@nyu.edu>
2019-12-05 15:18:38 -0800

    Implemented time selection component
    - modified server to allow for path parameter
    - removed unecessary files and dependencies
    - App makes request to get restaurantInfo on mount and sets state with the object
    - pass props to child components of app

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery';
 import DateSelection from './DateSelection.jsx';
 import TimeSelection from './TimeSelection.jsx';
 import PeopleSelection from './PeopleSelection.jsx';
@@ -7,17 +8,45 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      restaurantInfo: {},
+      showResults: false,
+    };
+  }
+
+
+  componentDidMount() {
+    const restaurantId = window.location.pathname;
+    $.ajax({
+      url: `/api${restaurantId}`,
+      method: 'GET',
+      success: (response) => {
+        console.log(response);
+        this.setState({
+          restaurantInfo: response,
+        });
+      },
+    });
   }
 
   render() {
+    const { restaurantInfo } = this.state;
+    const {
+      name,
+      open,
+      close,
+      min,
+      max,
+      dates,
+    } = restaurantInfo;
+
     return (
       <div>
-        <p>Make a Reservation</p>
+        <h2>Make a Reservation</h2>
         <form>
-          <div><DateSelection /></div>
-          <TimeSelection />
-          <PeopleSelection />
+          <div><DateSelection dates={dates} /></div>
+          <TimeSelection open={open} close={close} />
+          <PeopleSelection min={min} max={max} />
           <p><button type="submit">Find a Table</button></p>
         </form>
       </div>

a0b84ee76fd12077e0f5bf0f0f984a1ece2abb37
Laurence <ldn221@nyu.edu>
2019-12-05 22:59:33 -0800

    Refactor PeopleSelection to be functional component

--- client/PeopleSelection.jsx
+++ client/PeopleSelection.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 
-class PeopleSelection extends React.Component {
-  createSelections() {
-    const { min, max } = this.props;
+const PeopleSelection = (props) => {
+  const createSelections = () => {
+    const { min, max } = props;
     const children = [];
     for (let i = min; i <= max; i += 1) {
       if (i === 1) children.push(<option value={i}>{i} person</option>);
       else children.push(<option value={i}>{i} people</option>);
     }
     return children;
-  }
+  };
 
-  render() {
-    return (
-      <span>
-        <select name="people" id="PeopleSelect">
-          {this.createSelections()}
-        </select>
-      </span>
-    );
-  }
-}
-
+  return (
+    <span>
+      <select name="people" id="PeopleSelect">
+        {createSelections()}
+      </select>
+    </span>
+  );
+};
 
 export default PeopleSelection;
